9abe85e32a51ae88f83ef565ee44de4c1a38bcd7
codingxjourney <codingxjourney@gmail.com>
2021-10-24 20:43:21 +0530

    first commit

--- js/tut52.js
+++ js/tut52.js
@@ -2,5 +2,5 @@ console.log('This is js tutorial 52');
 // You have to crate an alarm clock in javascript (Use your creativity)
 // Allow user to set alarm for a certain time
 
-var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3');
+var audio = new Audio('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
 audio.play();
